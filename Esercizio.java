136dbd4e0133c5603a18ab70c01ebebc2232cd7e
stefanoantoniodonno <stefanoantonio.donno@marconibari.edu.it>
2025-04-10 17:22:41 +0000

    vettori ord

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,150 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
-// Classe principale, con metodo main
+
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+  
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void BubbleSort(int[] V, int N){
+        boolean trovato;
+        int i = 0;
+        int j;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        trovato = false;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        while(i < N && !trovato){
+            j = 0;
+            trovato = true;
+            while(j < (N - 1) - i){
+                if(V[j] > V[j+1]){
+                    int t = V[j];
+                    V[j] = V[j+1];
+                    V[j+1] = t;
+                    trovato = false;
+                }
+                j++;
+            }
+            i++; 
+        }
+
     }
+
+    public static void Merge(int[] V, int[] R, int numV, int numR, int[] V_merge){
+   
+        int i, j, k;
+
+        i = 0;
+        j = 0;
+        k = 0;
+
+        while((i < numV) && (j < numR)){
+            if(V[i] <= R[j]){
+                V_merge[k] = V[i]; 
+                i++;
+            }else{
+                V_merge[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < numV){
+            V_merge[k] = V[i];
+            k++;
+            i++;
+        }
+
+        while(j < numR){
+            V_merge[k] = R[j];
+            k++;
+            j++;
+        }
+
+    }
+
+    public static int Ricerca_binaria(int[] V, int N, int numero){
+        int inizio, fine, medio = 0, i = -1;
+        boolean trovato = false;
+
+        inizio = 0;
+        fine = N - 1;
+        while(inizio <= fine && !trovato){
+            medio = inizio + (fine - inizio) / 2;
+            if(V[medio] == numero){
+                trovato = true;
+                i = medio;
+            }else if(V[medio] < numero){
+                inizio = medio + 1;
+            }else{
+                fine = medio - 1;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String args[]){
+        
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+    
+        int N, num, numP, numD, index;
+
+        System.out.print("Inserire il numero di numeri da generare: ");
+        N = in.nextInt();
+
+        int[] temp_P = new int[N];
+        int[] temp_D = new int[N];
+
+        numD = 0;
+        numP = 0;
+
+        
+        for(int i = 0; i < N; ++i){
+            num = rand.nextInt(100);
+            if(num % 2 == 0){
+                temp_P[numP] = num;
+                numP ++;
+            }else{
+                temp_D[numD] = num;
+                numD++;
+            }
+        }
+
+        int[] P = new int[numP];
+        int[] D = new int[numD];
+
+        for (int i = 0; i < numP; i++) {
+            P[i] = temp_P[i];
+        }
+        
+        for (int i = 0; i < numD; i++) {
+            D[i] = temp_D[i];
+        }
+
+        BubbleSort(D, numD);
+        BubbleSort(P, numP);
+
+        int[] V_merge = new int[N];
+
+        Merge(D, P, numD, numP, V_merge);
+        System.out.println("Vettore unito e ordinato: ");
+        for(int i = 0; i < N; ++i){
+            System.out.println(i + ":" +  V_merge[i] + " ");
+        }
+
+        int numeroRicerca;
+
+        System.out.print("Inserire numero da ricercare nel vettore: ");
+        numeroRicerca = in.nextInt();
+
+        index = Ricerca_binaria(V_merge, N, numeroRicerca);
+
+        if(index != -1){
+            System.out.println("Il numero: " + V_merge[index] + " si trova nella posizione: " + index + "!");
+        }else{
+            System.out.println("Numero non trovato!");
+        }
+    }   
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
